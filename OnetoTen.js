8d942a97de6ac3807c86bc51a3af9ce52508ba1e
cutiegirl526 <kayladiniega@gmail.com>
2016-11-07 08:44:17 -1000

    Fishies

--- OnetoTen.js
+++ OnetoTen.js
@@ -1,5 +1,5 @@
 // Variables of the first part
-var hero = "Awkawrd guy";
+var hero = "Snezzing girl";
 var gender = "Female";
 var age = 26;
 var weapon = "runny nose";
@@ -30,7 +30,15 @@ if (age <= 18){
 
 // function for the age and sentence
 
-console.log(hero + " is our loveable hero, who " + xp + " with her favorite " + weapon + "!")
+console.log(hero + " is our loveable hero, who " + xp + " with her favorite trusty" + weapon + "!")
 
 // fishies
 
+var count;
+var animal = "Pitbull";
+
+// function
+
+function animalHouse(p1,p2){
+	var count = p1+p2;
+}
